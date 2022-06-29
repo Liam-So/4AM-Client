550374ed7c446994926f0c34c6f48f86c4efa25d
Liam So <Liam.So@dal.ca>
2022-06-29 01:13:21 -0300

    Text change

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -29,7 +29,7 @@ function Register() {
         <div className="w-full md:w-7/12 text-center md:text-left p-4">
           <div className="text-6xl font-medium">Sorry...</div>
           <div className="text-xl md:text-3xl font-medium mb-4">
-          Registrations are currently closed.
+            Registrations are currently closed.
           </div>
           <div className="text-lg mb-8">
             Stay tuned to our social media accounts or Registration announcements. Thank you for your interest!
@@ -41,31 +41,31 @@ function Register() {
 
   const RegisterCamp = () => (
     <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="font-sans bg-white">
-        <div>
-            <section className="bg-white mt-20 mb-20">
-                <div className="max-w-2xl px-6 text-center mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800">Registration now open!</h2>
-                    <p className="text-gray-600 mt-4">The 2021 4AM Camp will run the week of August 16-19 (Aug 20 will be a rain date -if it rains on one of the days from Monday to Thursday the camp will be pushed one day and we will go on Friday) at the Caledonia Jr High Outdoor Courts. The cost of the camp is $100. All proceeds from the camp go towards the Andrew Milner Memorial Scholarship.</p>
-                    <p className="text-gray-600 mt-4">Boys entering grade 9-10 this September will have a session at <strong>5-7 pm</strong> while boys entering grade 11-12 this September will have a session at <strong>7-9 pm</strong></p>
-                </div>
-            </section>
-          
-            <section className="pattern pb-56">
-                <div className="flex flex-col pb-16 md:px-12 lg:flex-row lg:px-60 xl:px-96">
-                {availableProducts.map((product) => (
-                    <div className="flex container mx-auto max-w-sm w-full p-4 sm:w-1/2">
-                      <RegisterItem product={product}/>
-                    </div>
-                  ))}
-                </div>
-            </section>
-        </div>
+      <div>
+        <section className="bg-white mt-20 mb-20">
+          <div className="max-w-2xl px-6 text-center mx-auto">
+            <h2 className="text-3xl font-semibold text-gray-800">Registration now open!</h2>
+            <p className="text-gray-600 mt-4">The 2022 4AM Camp will run the week of August 16-19 (Aug 20 will be a rain date -if it rains on one of the days from Monday to Thursday the camp will be pushed one day and we will go on Friday) at the Caledonia Jr High Outdoor Courts. The cost of the camp is $100. All proceeds from the camp go towards the Andrew Milner Memorial Scholarship.</p>
+            <p className="text-gray-600 mt-4">Boys entering grade 9-10 this September will have a session at <strong>5-7 pm</strong> while boys entering grade 11-12 this September will have a session at <strong>7-9 pm</strong></p>
+          </div>
+        </section>
+
+        <section className="pattern pb-56">
+          <div className="flex flex-col pb-16 md:px-12 lg:flex-row lg:px-60 xl:px-96">
+            {availableProducts.map((product) => (
+              <div className="flex container mx-auto max-w-sm w-full p-4 sm:w-1/2">
+                <RegisterItem product={product} />
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
     </motion.main>
   );
 
   return (
     <div>
-      <Topbar transparent={true}/>
+      <Topbar transparent={true} />
       {availableProducts.length ? <RegisterCamp /> : <EmptyCamp />}
     </div>
   );
