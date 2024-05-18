039a9d0bf1b6ed7650cea841050e941f9f6fcc66
4ambasketball <4AM.information@gmail.com>
2024-05-18 19:02:08 -0300

    Trying to fix award formm v4

--- src/components/Pages/Scholarship/Scholarship.js
+++ src/components/Pages/Scholarship/Scholarship.js
@@ -232,7 +232,7 @@ function Scholarship() {
                             rows="10"
                             className="txt shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md whitespace-pre-wrap"
                             placeholder="Your response here..."
-                            maxLength="5000"
+                            maxLength="3500"
                             required
                           ></textarea>
                         </div>
